d5d9a57c869f6379c248e71f4eae7c076de53745
Feri1996HUN <67258569+Feri1996HUN@users.noreply.github.com>
2021-11-29 14:59:56 +0100

    Add files via upload

---
+++ Webshop/kapcsolat.php
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webshop</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!-- Jumbotron fejléc -->
+
+<body style="background-color:red;">
+    <div class="container-fluid text-center">
+        <div class="jumbotron bg-warning">
+            <h1><b>PIRAMIS ITALKERESKEDÉS</b></h1>
+
+            <!-- MENÜ -->
+
+            <p><nav class="navbar navbar-expand-lg navbar-light bg-warning">
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                        MENÜ
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item " href="index.php">Főoldal</a>
+                        <a class="dropdown-item disabled" href="kapcsolat.php">Kapcsolat</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="webshop.php">WEBSHOP</a>
+                    </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Kosár:</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="kosar" name="kosar">0</a>
+                    </li>
+                </ul>
+                </div>
+            </nav></p>
+        </div> 
+    </div>
+
+
+    
+</body>
+</html>
